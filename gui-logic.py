905139109f7ee486e2f16e62f731778a768fbc6a
Carlos Gil <cgil1210@gmail.com>
2014-01-14 19:19:41 -0500

    code trying to build ROI and mask to get rect color intensity

--- gui-logic.py
+++ gui-logic.py
@@ -1,1 +1,38 @@
 #!/usr/bin/env python
+
+import cv2
+import numpy as np
+
+def logic(coords):
+	pass
+
+
+def getImage():
+	c = cv2.VideoCapture(0)
+	_,f = c.read()
+	y1 = 0
+	y2 = 100
+	x1 = 0
+	x2 = 100
+	mask = np.zeros(f.shape, dtype=np.uint8)
+	roi_corners = np.array([[(10,10), (300,300), (10,300)]], dtype=np.int32)
+	white = (255, 255, 255)
+	cv2.fillPoly(mask, roi_corners, white)
+
+	# masked_image = cv2.bitwise_and(f, mask)
+
+	masked_image = cv2.mean(f, mask)
+	# masked_image = cv2.mean(f, roi_corners)
+	
+	cv2.imshow('masked image', masked_image)
+	cv2.waitKey()
+	cv2.destroyAllWindows()
+	c.release()
+	# old_roi = cvGetImageROI(img); 
+	# cvSetImageROI(img, cvRect(x,y,width,height)); 
+	# CvScalar c = cvAvg(img); 
+	# cvSetImageROI(img,old_roi); // reset old roi 
+	# cv2.cv.SetImageROI
+
+getImage()
+

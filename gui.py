dfabcee9ed2f87721229a792a2e71634e102e27b
kswilster <keithwilliams5432@gmail.com>
2014-01-16 20:51:01 -0500

    added basic gui

--- gui.py
+++ gui.py
@@ -1,1 +1,46 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainwindow.ui'
+#
+# Created: Thu Jan 16 20:38:44 2014
+#      by: pyside-uic 0.2.15 running on PySide 1.2.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide import QtCore, QtGui
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(716, 500)
+        self.centralWidget = QtGui.QWidget(MainWindow)
+        self.centralWidget.setObjectName("centralWidget")
+        self.videoFrame = QtGui.QLabel(self.centralWidget)
+        self.videoFrame.setGeometry(QtCore.QRect(40, 30, 301, 281))
+        self.videoFrame.setObjectName("videoFrame")
+        MainWindow.setCentralWidget(self.centralWidget)
+        self.mainToolBar = QtGui.QToolBar(MainWindow)
+        self.mainToolBar.setObjectName("mainToolBar")
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.mainToolBar)
+        self.statusBar = QtGui.QStatusBar(MainWindow)
+        self.statusBar.setObjectName("statusBar")
+        MainWindow.setStatusBar(self.statusBar)
+        self.menuBar = QtGui.QMenuBar(MainWindow)
+        self.menuBar.setGeometry(QtCore.QRect(0, 0, 716, 21))
+        self.menuBar.setObjectName("menuBar")
+        self.menuRoomal = QtGui.QMenu(self.menuBar)
+        self.menuRoomal.setObjectName("menuRoomal")
+        MainWindow.setMenuBar(self.menuBar)
+        self.actionFile = QtGui.QAction(MainWindow)
+        self.actionFile.setObjectName("actionFile")
+        self.menuBar.addAction(self.menuRoomal.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
+        self.videoFrame.setText(QtGui.QApplication.translate("MainWindow", "TextLabel", None, QtGui.QApplication.UnicodeUTF8))
+        self.menuRoomal.setTitle(QtGui.QApplication.translate("MainWindow", "File", None, QtGui.QApplication.UnicodeUTF8))
+        self.actionFile.setText(QtGui.QApplication.translate("MainWindow", "File", None, QtGui.QApplication.UnicodeUTF8))
+
